2c6b97b32f5fa435d2f4e82ae25cea7704c54032
beataj <beata@emc2wave.com>
2022-03-02 00:00:03 -0800

    add MatNativeDateModule datapicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,12 +3,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
 const material = [
   MatButtonModule,
   MatIconModule,
   MatInputModule,
   MatFormFieldModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
 ];
 
 @NgModule({
