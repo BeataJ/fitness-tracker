d26b2bf78b9732a6fa72faae682b5730991d4acd
beataj <beata@emc2wave.com>
2022-03-07 14:25:12 -0800

    bind formGroup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,7 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup | undefined;
+  loginForm!: FormGroup;
 
   constructor() { }
 
