85610fede4cae2814408ef762547d51b3d79d1de
beataj <beata@emc2wave.com>
2022-03-26 18:57:09 -0700

    add router to auth.service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
+
 import { AuthData } from "./auth-data.model";
 import { User } from "./user.model";
 
@@ -10,12 +12,15 @@ export class AuthService {
   authChange = new Subject<boolean>()
   private user: User | undefined;
 
+  constructor(private router: Router) {}
+
   registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString(),
     };
-    this.authChange.next(true)
+    this.authChange.next(true);
+    this.router.navigate(['/training'])
   }
 
   login(authData: AuthData) {
@@ -24,11 +29,13 @@ export class AuthService {
       userId: Math.round(Math.random() * 10000).toString(),
     };
     this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 
   logout() {
     this.user = undefined;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
