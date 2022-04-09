9e1f608c58170ad9cf7031178157e582f88ba8e2
beataj <beata@emc2wave.com>
2022-04-09 00:14:46 -0700

    add env..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyDjvRJDBNeKrTDL15j6Teiv2-wFpml64Ak',
+  authDomain: 'ng-fitness-tracker-23eb4.firebaseapp.com',
+  projectId: 'ng-fitness-tracker-23eb4',
+  storageBucket: 'ng-fitness-tracker-23eb4.appspot.com',
+  messagingSenderId: '735482627042',
+  appId: '1:735482627042:web:1106205f6e0d091d04efbb',
 };
 
 /*
