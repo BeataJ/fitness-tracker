d7566164ac867ce463e59672d4bac2352a116071
beataj <beata@emc2wave.com>
2022-04-21 23:29:39 -0700

    add map

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {   Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+
 
 
 @Component({
@@ -14,30 +15,36 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  @Output() trainingStart = new EventEmitter<void>()
-  exercises!: Observable<any>;
+  @Output() trainingStart = new EventEmitter<void>();
+  exercises!: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
     private db: AngularFirestore
-    ) {}
+  ) {}
 
   ngOnInit(): void {
-    this.exercises = this.db.collection('availableExercises').snapshotChanges()
-    .pipe(
-      map(docArray => {
-        return docArray.map(doc => {
-          return {
-            id: doc.payload.doc.id,
-            doc: doc.payload.doc.data()
-          }
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map((docArray: { payload: { doc: { id: any; data: () => any; }; }; }[]) => {
+          return docArray.map((doc: { payload: { doc: { id: any; data: () => any; }; }; }) => {
+            return {
+
+              id: doc.payload.doc.id,
+              calories: doc.payload.doc.data().calories,
+              name: doc.payload.doc.data().name,
+              duration: doc.payload.doc.data().duration,
+
+            };
+          });
         })
-      })
-    )
+      )
 
   }
 
   onStartTraining(form: NgForm) {
-    this.trainingService.startExercise(form.value.exercise)
+    this.trainingService.startExercise(form.value.exercise);
   }
 }
