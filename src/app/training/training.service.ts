90e817a8d22ff52adb87a990dba06e802bfedb64
beataj <beata@emc2wave.com>
2022-04-21 23:37:46 -0700

    change on fetchAvailableExercises()

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,18 +8,11 @@ import { Exercise } from "./exercise.model";
 })
 export class TrainingService {
   exerciseChange = new Subject<Exercise | null>();
-
-  private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
-  ];
-
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise | any = [];
   private exercises: Exercise[] = [];
 
-  getAvailableExercises() {
+  fetchAvailableExercises() {
     return this.availableExercises.slice();
   }
 
@@ -34,7 +27,7 @@ export class TrainingService {
     this.exercises.push({
       ...this.runningExercise,
       date: new Date(),
-      state: 'completed'
+      state: 'completed',
     });
     this.runningExercise = null;
     this.exerciseChange.next(null);
