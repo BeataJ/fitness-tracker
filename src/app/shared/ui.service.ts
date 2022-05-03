3a2110409da25258445d52121a0f377fc1438d96
beataj <beata@emc2wave.com>
2022-05-02 20:29:50 -0700

    add @Injectable({   providedIn: 'root' })

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,5 +1,9 @@
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class UIService {
   logingStateChanged = new Subject<boolean>()
 }
