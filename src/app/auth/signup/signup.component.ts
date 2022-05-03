0b0399f7a0f67fd1a823b69f24c877987e74cd38
beataj <beata@emc2wave.com>
2022-05-02 21:20:15 -0700

    add uiService to ngOnInit

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
@@ -12,7 +12,7 @@ import { AuthService } from '../auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   maxDate;
   isLoading = false;
   private loadingSubs: Subscription = new Subscription;
@@ -25,12 +25,20 @@ export class SignupComponent implements OnInit {
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadingSubs = this.uiService.logingStateChanged.subscribe(isloading => {
+      this.isLoading = isloading;
+    })
+  }
 
   onSubmit(form: NgForm) {
     this.authService.registerUser({
       email: form.value.email,
       password: form.value.password
     })
+  }
+
+  ngOnDestroy(): void {
+      this.loadingSubs.unsubscribe();
   }
 }
