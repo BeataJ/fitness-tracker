fce98785ff41c9cd501aeb0ab9012d95b168fe6a
beataj <beata@emc2wave.com>
2022-05-04 14:49:07 -0700

    add snackbar to uiservice

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UIService {
   logingStateChanged = new Subject<boolean>()
+
+  constructor(private snackbar: MatSnackBar) {}
+
+  showSnackbar(message: any, action: any, duration: any) {
+    this.snackbar.open(message, action, {
+      duration: duration
+    })
+  }
 }
