55d25831b9d08e18bda19b842e2e947c2359b6dc
beataj <beata@emc2wave.com>
2022-05-10 20:48:24 -0700

    add authRoutingModule to authModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from "@angular/core";
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { ReactiveFormsModule } from "@angular/forms";
-import { SharedModule } from "../shared/shared.module";
-
 
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
+import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
   imports: [
     ReactiveFormsModule,
     AngularFirestoreModule,
-    SharedModule],
+    SharedModule,
+    AuthRoutingModule
+  ],
   exports: [],
 })
 export class AuthModule {}
