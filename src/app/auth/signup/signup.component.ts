ab126328a7122d35eb51a6d864fec940cfcde953
beataj <beata@emc2wave.com>
2022-05-17 14:37:18 -0700

    add store with isLoding to signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { UIService } from 'src/app/shared/ui.service';
 import { AuthService } from '../auth.service';
-
+import * as fromRoot from '../../app.reducer';
+import { Store } from '@ngrx/store';
 
 
 @Component({
@@ -12,23 +13,25 @@ import { AuthService } from '../auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit, OnDestroy {
+export class SignupComponent implements OnInit {
   maxDate;
-  isLoading = false;
+  isLoading$ = new Observable<boolean>();
   private loadingSubs: Subscription = new Subscription;
 
   constructor(
     private authService: AuthService,
-    private uiService: UIService
+    private uiService: UIService,
+    private store: Store<fromRoot.State>
     ) {
     this.maxDate = new Date;
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
   ngOnInit(): void {
-    this.loadingSubs = this.uiService.logingStateChanged.subscribe(isloading => {
-      this.isLoading = isloading;
-    })
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading)
+    // this.loadingSubs = this.uiService.logingStateChanged.subscribe(isloading => {
+    //   this.isLoading = isloading;
+    // })
   }
 
   onSubmit(form: NgForm) {
@@ -38,10 +41,10 @@ export class SignupComponent implements OnInit, OnDestroy {
     })
   }
 
-  ngOnDestroy(): void {
-    if(this.loadingSubs){
-      this.loadingSubs.unsubscribe();
-    }
+  // ngOnDestroy(): void {
+  //   if(this.loadingSubs){
+  //     this.loadingSubs.unsubscribe();
+  //   }
 
-  }
+  // }
 }
