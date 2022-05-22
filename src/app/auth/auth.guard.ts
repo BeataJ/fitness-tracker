35afaf946adfcd7b28d091f905ad58137fff7e78
beataj <beata@emc2wave.com>
2022-05-21 17:45:38 -0700

    add empty training reducer actions

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,8 +1,7 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot} from "@angular/router";
+import { ActivatedRouteSnapshot, CanLoad, Route, Router, RouterStateSnapshot} from "@angular/router";
 import { Store } from '@ngrx/store';
 
-import { AuthService } from "./auth.service";
 import * as fromRoot from '../app.reducer';
 import { take } from "rxjs";
 
