4d74ef6fd2dd4655cd5c3796f9bff632335be9b9
beataj <beata@emc2wave.com>
2022-05-28 16:56:19 -0700

    add store to past-training

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,30 +1,35 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import * as fromTraining from '../training.reducer'
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css'],
 })
-export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingComponent implements OnInit, AfterViewInit{
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
-  private exChangeSubscription: Subscription = new Subscription;
+
 
   @ViewChild(MatSort)
   sort: MatSort = new MatSort();
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private store: Store<fromTraining.State>
+    ) {}
 
   ngOnInit() {
-    this.exChangeSubscription = this.trainingService.finishedExercisesChange.subscribe(
+    this.store.select(fromTraining.getFinishedExercises).subscribe(
       (exercises: Exercise[]) => {
         this.dataSource.data = exercises;
       }
@@ -42,10 +47,5 @@ export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  ngOnDestroy(): void {
-    if(this.exChangeSubscription) {
-      this.exChangeSubscription.unsubscribe();
-    }
 
-  }
 }
